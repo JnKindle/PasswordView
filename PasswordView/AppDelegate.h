9174afb8fe9be4b7d67b31679c32ff0ef4bbdc20
JnKindle <Jn_never2013@163.com>
2018-08-03 12:25:11 +0800

    Initial Commit

---
+++ PasswordView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PasswordView
+//
+//  Created by Jn_Kindle on 2018/8/3.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PasswordView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PasswordView
+//
+//  Created by Jn_Kindle on 2018/8/3.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
